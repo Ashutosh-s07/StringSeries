4f1d42937b960bc2b4123bad858a6752a31864c8
Ashutosh Sahu <sahuashutosh248@gmail.com>
2024-02-21 12:40:28 +0530

    String Series

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,8 +1,30 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GroupAnagrams {
-    public static void groupAnagrams(String[] strs) {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+
+    public static void groupAnagrams2(String[] strs) {
         
         List<List<String>> resuList = new ArrayList<>();
         
